f37ff38740d8ec088fda2043187458a4530694c7
nrymarz <nrymarz@gmail.com>
2020-12-05 01:24:51 -0600

    message

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
-
+    get '/tweets' do 
+        if Helpers.logged_in?(session)
+            erb :'tweets/new'
+        else
+            redirect '/login'
+        end
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,48 @@
 class UsersController < ApplicationController
+    get '/signup' do 
+        if !Helpers.logged_in?(session)
+            erb :'/users/signup'
+        else
+            redirect '/tweets'
+        end
+    end
 
+    get '/login' do
+        if !Helpers.logged_in?(session)
+            erb :'/users/login'
+        else
+            redirect '/tweets'
+        end
+    end
 
+    post '/signup' do
+        user = User.new(params)
+        if user.username.size > 0 && user.email.size >0 && user.save
+            session[:user_id] = user.id 
+            redirect "/tweets"
+        else
+            redirect '/users/signup'
+        end
+    end
+
+    post '/login' do 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/tweets'
+        else
+            @error = "Could not login"
+            erb :'/users/login'
+        end
+    end
+
+    get '/logout' do 
+        if Helpers.logged_in?(session)
+            session.clear
+            redirect '/login'
+        else
+            redirect '/'
+        end
+    end
+  
 end
